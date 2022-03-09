1205889ea66e5400dd2473324da8453a10ecc31a
JV-Amorim <joaovitoramorimx@gmail.com>
2022-03-09 19:23:19 -0300

    fix: natural selection by binary tournament

--- src/natural_selection.py
+++ src/natural_selection.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 from config import INDIVIDUAL_LENGTH, NATURAL_SELECTION_METHOD, POPULATION_SIZE
 from natural_selection_methods import NaturalSelectionMethods
+from individual import calculate_individual_fitness
 
 
 def select_individuals_from_population(population):
@@ -25,13 +26,28 @@ def _select_individuals_with_tournament_method(population):
   selected_individuals = np.empty((POPULATION_SIZE, INDIVIDUAL_LENGTH))
 
   for i in range(0, POPULATION_SIZE, 2):
-    first_individual_index = random.randint(0, POPULATION_SIZE - 1)
-    second_individual_index = random.randint(0, POPULATION_SIZE - 1)
+    first_individual = _select_single_individual_with_tournament_method(population)
+    second_individual = _select_single_individual_with_tournament_method(population)
 
-    selected_individuals[i] = population[first_individual_index]
-    selected_individuals[i + 1] = population[second_individual_index]
+    selected_individuals[i] = first_individual
+    selected_individuals[i + 1] = second_individual
 
   return selected_individuals
+
+def _select_single_individual_with_tournament_method(population):
+  first_individual_index = random.randint(0, POPULATION_SIZE - 1)
+  second_individual_index = random.randint(0, POPULATION_SIZE - 1)
+
+  first_individual = population[first_individual_index]
+  second_individual = population[second_individual_index]
+
+  first_individual_fitness = calculate_individual_fitness(first_individual)
+  second_individual_fitness = calculate_individual_fitness(second_individual)
+
+  if first_individual_fitness >= second_individual_fitness:
+    return first_individual
+  
+  return second_individual
 
 
 def _select_individuals_with_uniform_method(population):
