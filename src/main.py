45d08d5bfa5e750ae27b9d857e8a3ef6a10a9aae
JV-Amorim <joaovitoramorimx@gmail.com>
2022-03-10 19:39:05 -0300

    feature: get_individuals_to_the_next_generation function implemented

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 from config import NUMBER_OF_GENERATIONS
+from individual import calculate_individual_fitness
 from natural_selection import select_individuals_to_reproduction
-from population import generate_initial_population, get_individuals_to_the_next_generation
+from population import find_best_individual_in_current_population, generate_initial_population, get_individuals_to_the_next_generation
 from reproduction import reproduce_population_and_get_the_children
 
 
-if __name__ == '__main__':
+def execute_genetic_algorithm_and_get_best_individual():
   current_population = generate_initial_population()
 
-  for population_index in range(NUMBER_OF_GENERATIONS):
+  for _ in range(NUMBER_OF_GENERATIONS):
     selected_individuals = select_individuals_to_reproduction(current_population)
     population_children = reproduce_population_and_get_the_children(selected_individuals)
     new_population = get_individuals_to_the_next_generation(current_population, population_children)
     current_population = new_population
+
+  return find_best_individual_in_current_population(current_population)
+
+
+if __name__ == '__main__':
+  best_individual = execute_genetic_algorithm_and_get_best_individual()
+  best_individual_fitness = calculate_individual_fitness(best_individual)
+  print('Fitness of the best individual of the last generation: ' + str(best_individual_fitness))
--- src/population.py
+++ src/population.py
@@ -1,6 +1,6 @@
 import numpy as np
 from config import INDIVIDUAL_LENGTH, POPULATION_SIZE
-from individual import generate_random_individual
+from individual import calculate_individual_fitness, generate_random_individual
 
 
 def generate_initial_population():
@@ -12,5 +12,37 @@ def generate_initial_population():
   return population
 
 
+# The following function uses the elitism method.
 def get_individuals_to_the_next_generation(current_population, population_children):
-  raise ValueError('Implementation missing.')
+  best_individual = find_best_individual_in_current_population(current_population) 
+  worst_child_index = _find_index_of_worst_child_in_current_children(population_children)
+
+  population_children[worst_child_index] = best_individual
+
+  return population_children
+
+def find_best_individual_in_current_population(current_population):
+  best_individual = current_population[0]
+  best_individual_fitness = calculate_individual_fitness(best_individual)
+
+  for current_individual in current_population:
+    current_individual_fitness = calculate_individual_fitness(current_individual)
+
+    if current_individual_fitness > best_individual_fitness:      
+      current_individual = best_individual
+      best_individual_fitness = current_individual_fitness
+
+  return best_individual
+
+def _find_index_of_worst_child_in_current_children(population_children):
+  index_of_worst_child = 0
+  worst_child_fitness = calculate_individual_fitness(population_children[0])
+
+  for current_index in range(0, POPULATION_SIZE):
+    current_child_fitness = calculate_individual_fitness(population_children[current_index])
+
+    if current_child_fitness < worst_child_fitness:
+      index_of_worst_child = current_index
+      worst_child_fitness = current_child_fitness
+
+  return index_of_worst_child
